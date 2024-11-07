03808ac9331c6ab1055f98ed9575f71fd5c318f6
vinwap07 <adelinazakirova24@gmail.com>
2024-11-07 09:56:14 +0300

    add form

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,8 +2,18 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main() 
     {
-        Console.WriteLine("Hello, World!");
+		string name = Console.ReadLine();
+		int age = int.Parse(Console.ReadLine());
+		int course = int.Parse(Console.ReadLine());
+		string groupNum = Console.ReadLine();
+		bool hasPet = false;
+        Console.WriteLine("Анкета студента:");
+		Console.WriteLine("Вас зовут " + name);
+		Console.WriteLine("Вам " + age + " лет");
+		Console.WriteLine("Вы на " + course + " курсе");
+		Console.WriteLine("В группе " + groupNum);
+		Console.WriteLine("Есть ли у вас домашний питомец: " + hasPet);
     }
 }
