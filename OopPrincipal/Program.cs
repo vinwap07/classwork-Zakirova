0c2187dabba94bd331415409144f6d6cf817fd5b
vinwap07 <adelinazakirova24@gmail.com>
2024-12-07 09:04:02 +0300

    add Student

---
+++ OopPrincipal/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OopPrincipal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var student = new Student("Kislov Kirill");
+        var student2 = new Student("Kislov Kirill", TypeOfStudy.University);
+        var student3 = new Student("LOLOLO")
+        {
+            age = 18,
+            endStudy = DateOnly.Parse("2025-05-31"),
+            startStudy = DateOnly.Parse("2024-09-01"),
+        };
+
+        student3.name = "KUKU";
+        student3.age = -18;
+        student3.endStudy = DateOnly.Parse("2125-05-31");
+
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ OopPrincipal/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace OopPrincipal;
+
+public class Student
+{
+    public string name;
+    public int age;
+    public TypeOfStudy typeOfStudy;
+    public DateOnly startStudy;
+    public DateOnly endStudy;
+
+    public Student(string name)
+    {
+        this.name = name;
+    }
+
+    public Student(string name, TypeOfStudy typeOfStudy) : this(name)
+    {
+        this.typeOfStudy = typeOfStudy;
+    }
+}
+
+public enum TypeOfStudy
+{
+    School,
+    University,
+    College
+}
